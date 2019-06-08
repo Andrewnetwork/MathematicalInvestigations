40c708d589969f5b5f8ec9befd3f80457fc3eab9
Andrew Ribeiro <andrewnetwork@gmail.com>
2019-06-08 13:15:53 -0400

    Silly

--- Probability/cuckooClockCausality.py
+++ Probability/cuckooClockCausality.py
@@ -1,35 +1,65 @@
+# cuckooClockCausality.py
+# Andrew Ribeiro 
+# June 2019 
+
 import numpy as np
 from IPython.display import clear_output
 import time
 from typing import Callable
 
-class Button: 
+class CausalityModel:
+    def causalViolation(self,sample)->bool:
+        """
+        Checks if given a sample, a 2xN boolean matrix, contains a causal violation. 
+        Namely, it check if the casual signal occurs before event signal. 
+        """
+        return True
+
+        
+
+class Button:
     def __init__(self,clickTime:Callable[[int],bool]):
-        # A bool
+        """
+        Args: 
+        - clickTime(time) -> button is pressed at the given time
+        """
         self.clickTime = clickTime
-        self.time = 0
-    def sample(self,endTime):
+    def sample(self,endTime)->np.array:
+        """
+        Produces a boolean array by evaluating clickTime on [0,endTime).
+        """
         ls = []
         for i in range(endTime):
             ls.append(self.clickTime(i))
         return np.array(ls)
 
 class CuckooClock: 
+    """
+    A class initialized 
+    """
     def __init__(self,isOut:Callable[[int,Button],bool], button:Button):
+        """
+        Args: 
+        - isOut(time,button) -> cuckoo bird is out of the clock. 
+        """
         # A bool
         self.isOut = isOut
         self.button = button
-        self.time = 0
+        self.initClock()
 
     def tick(self):
         self.time += 1
     
     def run(self,speed=.5,timeLimit=100):
-        for i in range(timeLimit):
-            clear_output()
+        self.initClock()
+        for _ in range(timeLimit):
+            clear_output(True)
             self.tick()
             if self.isOut(self.time,self.button):
-                print("{0} Coo Coo!".format(self.time))
+                if self.button.clickTime(self.time):
+                    print("{0} Coo Coo! (button pressed)".format(self.time))
+                else:
+                    print("{0} Coo Coo!".format(self.time))
             else: 
                 print("{0}".format(self.time))
             time.sleep(speed)
@@ -37,14 +67,19 @@ class CuckooClock:
     def sample(self,timeLimit=100) -> np.array:
         clockData = []
         buttonData = [] 
-        
-        for i in range(timeLimit):
+        self.initClock()
+
+        for _ in range(timeLimit):
             self.tick()
             clockData.append(self.isOut(self.time,self.button))
             buttonData.append(self.button.clickTime(self.time))
         
         return np.stack((clockData,buttonData),0)
 
+    def initClock(self):
+        self.time = 0
 
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
